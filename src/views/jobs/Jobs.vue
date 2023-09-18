ca47f50542ff1468b76888ede0c9676f283dea4a
Dave Get <daveget2020@gmail.com>
2023-09-18 14:57:18 +0200

    Conditionally Showing Data

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,22 +1,27 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
   </div>
+  <div v-else><p>Loading Jobs ...</p></div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      jobs: [
-        { title: "J1", id: 1, details: "111111" },
-        { title: "J2", id: 2, details: "222222" },
-        { title: "J3", id: 3, details: "333333" },
-      ],
+      jobs: [],
     };
+  },
+  mounted() {
+    fetch("  http://localhost:3000/jobs")
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
